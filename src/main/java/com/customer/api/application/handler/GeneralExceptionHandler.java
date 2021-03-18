9041e48ad94fa3a11f7cedca72195e3a53200187
Mateus Fiuza <mateus130@hotmail.com>
2021-03-18 02:54:23 -0300

    ajuste error exceptions

--- src/main/java/com/customer/api/application/handler/GeneralExceptionHandler.java
+++ src/main/java/com/customer/api/application/handler/GeneralExceptionHandler.java
@@ -7,8 +7,14 @@ import com.customer.api.domain.service.exception.ProductNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.bind.support.WebExchangeBindException;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class GeneralExceptionHandler {
@@ -23,6 +29,22 @@ public class GeneralExceptionHandler {
     protected ResponseEntity<ErrorResponse> handleUnregisteredCustomer(final CustomerNotFoundException ex) {
         final var apiError = new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
         return new ResponseEntity<>(apiError, new HttpHeaders(), apiError.getStatus());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    @ExceptionHandler(WebExchangeBindException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(final WebExchangeBindException ex) {
+        return new ResponseEntity<>(processFieldErrors(ex.getFieldErrors()), HttpStatus.BAD_REQUEST);
+    }
+
+    private ErrorResponse processFieldErrors(final List<FieldError> fieldErrors) {
+        final var error = new ErrorResponse(HttpStatus.BAD_REQUEST, "validation error");
+        fieldErrors.forEach((fieldError) -> {
+            error.addFieldError(fieldError.getField(), fieldError.getDefaultMessage());
+        });
+
+        return error;
     }
 
 }
